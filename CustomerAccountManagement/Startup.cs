78421ded669ae5e3259006f87c593fed044f38f2
Poy Chang <poypost@gmail.com>
2015-11-09 20:50:44 +0800

    專案建立

---
+++ CustomerAccountManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerAccountManagement.Startup))]
+namespace CustomerAccountManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
