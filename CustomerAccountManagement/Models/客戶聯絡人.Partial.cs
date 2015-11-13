87e7d467bd941002256d6b29472739eedca107e0
Poy Chang <poypost@gmail.com>
2015-11-13 13:24:50 +0800

    建立檢視客戶聯絡人及帳戶數量清單功能

--- CustomerAccountManagement/Models/客戶聯絡人.Partial.cs
+++ CustomerAccountManagement/Models/客戶聯絡人.Partial.cs
@@ -3,40 +3,51 @@ namespace CustomerAccountManagement.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+    using System.Linq;
+
     [MetadataType(typeof(客戶聯絡人MetaData))]
     public partial class 客戶聯絡人
     {
     }
-    
-    public partial class 客戶聯絡人MetaData
+
+    public partial class 客戶聯絡人MetaData : IValidatableObject
     {
         [Required]
         public int Id { get; set; }
         [Required]
         public int 客戶Id { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 職稱 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 姓名 { get; set; }
 
         [EmailAddress(ErrorMessage = "請檢查email格式")]
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [Required]
         public string Email { get; set; }
-        
-        [Mobile]
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        //[Mobile]
+        [RegularExpression(@"\d{4}-\d{6}", ErrorMessage = "手機格式必須為1234-123456")]
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 手機 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 電話 { get; set; }
         public Nullable<bool> 是否已刪除 { get; set; }
-    
+
         public virtual 客戶資料 客戶資料 { get; set; }
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            if (Email.Contains("ppp"))
+            {
+                yield return new ValidationResult("test", new[] { "Email" });
+            }
+            throw new NotImplementedException();
+        }
     }
 }
--- CustomerAccountManagement/文件/dbo.vw客戶聯絡人及帳戶數量.sql
+++ CustomerAccountManagement/文件/dbo.vw客戶聯絡人及帳戶數量.sql
@@ -9,11 +9,14 @@ LEFT JOIN (
 	SELECT 客戶Id
 		,count(*) AS 銀行帳戶數量
 	FROM 客戶銀行資訊
+	WHERE ISNULL(客戶銀行資訊.是否已刪除, 0) = 0
 	GROUP BY 客戶Id
 	) AS 客戶銀行資訊 ON 客戶資料.Id = 客戶銀行資訊.客戶Id
 LEFT JOIN (
 	SELECT 客戶Id
 		,count(*) AS 聯絡人數量
 	FROM 客戶聯絡人
+	WHERE ISNULL(客戶聯絡人.是否已刪除, 0) = 0
 	GROUP BY 客戶Id
 	) AS 客戶聯絡人 ON 客戶資料.Id = 客戶聯絡人.客戶Id
+WHERE ISNULL(客戶資料.是否已刪除, 0) = 0
