4cabb44541a580d8bce7f3dc70e3566749b3c2d4
Poy Chang <poypost@gmail.com>
2015-11-14 00:32:36 +0800

    驗證欄位Email不可重複

--- CustomerAccountManagement/Models/客戶聯絡人.Partial.cs
+++ CustomerAccountManagement/Models/客戶聯絡人.Partial.cs
@@ -25,12 +25,13 @@ namespace CustomerAccountManagement.Models
         [Required]
         public string 姓名 { get; set; }
 
+        [CustomerAccountManagement.Models.Attribute.Email]
         [EmailAddress(ErrorMessage = "請檢查email格式")]
         [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [Required]
         public string Email { get; set; }
 
-        //[Mobile]
+        [Mobile]
         [RegularExpression(@"\d{4}-\d{6}", ErrorMessage = "手機格式必須為1234-123456")]
         [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 手機 { get; set; }
@@ -41,13 +42,15 @@ namespace CustomerAccountManagement.Models
 
         public virtual 客戶資料 客戶資料 { get; set; }
 
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (Email.Contains("ppp"))
+            foreach (var item in 客戶資料.客戶聯絡人)
             {
-                yield return new ValidationResult("test", new[] { "Email" });
+                if (item.Email.Equals(this.Email))
+                {
+                    yield return new ValidationResult("電子郵件重複", new[] { "Email" });
+                }
             }
-            throw new NotImplementedException();
         }
     }
 }
