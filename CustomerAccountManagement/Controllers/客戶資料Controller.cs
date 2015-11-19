7b0b68a4373d2b6c474b16b53680e0e891f1583a
Poy Chang <poypost@gmail.com>
2015-11-19 22:42:55 +0800

    在客戶資料中增加客戶分類的欄位

--- CustomerAccountManagement/Controllers/客戶資料Controller.cs
+++ CustomerAccountManagement/Controllers/客戶資料Controller.cs
@@ -63,7 +63,7 @@ namespace CustomerAccountManagement.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,客戶分類,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
         {
             if (ModelState.IsValid)
             {
@@ -109,7 +109,7 @@ namespace CustomerAccountManagement.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,客戶分類,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
         {
             if (ModelState.IsValid)
             {
--- CustomerAccountManagement/Models/客戶資料.Partial.cs
+++ CustomerAccountManagement/Models/客戶資料.Partial.cs
@@ -37,6 +37,9 @@ namespace CustomerAccountManagement.Models
         [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
         public string Email { get; set; }
         public Nullable<bool> 是否已刪除 { get; set; }
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
+        public string 客戶分類 { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
